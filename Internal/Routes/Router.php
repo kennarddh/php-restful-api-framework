8992c0e2372306b593bd1564ac4a1b0845c21132
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-12-11 23:47:24 +0700

    docs(comments): Internal router

--- Internal/Routes/Router.php
+++ Internal/Routes/Router.php
@@ -9,13 +9,20 @@ use Internal\Http\Response;
 use Internal\Middlewares\ResolveMiddleware;
 use Application\Routes\Routes;
 
+/**
+ * Main router
+ */
 class Router
 {
+	/**
+	 * Register router
+	 */
 	public static function Register()
 	{
 		$url = Utils::GetUrl();
 		$method = $_SERVER['REQUEST_METHOD'];
 
+		// Create user defined route instance
 		$routes = new Routes();
 
 		foreach ($routes->paths as $route) {
@@ -25,10 +32,11 @@ class Router
 				Utils::IsUrlMatch($route->path, $url, $params) &&
 				($method === $route->method || $route->method === 'ALL')
 			) {
-				// match
+				// If url match with one of user defined paths in routes call defined controller method
 
 				$request = new Request($params);
 				$response = new Response(function (Response $response) use ($request, $route) {
+					// Run all after middleware sequentially
 					foreach ($route->middlewares->after as $middleware) {
 						$middlewareFunctionName = ResolveMiddleware::ResolveFunctionName($middleware);
 
@@ -36,16 +44,19 @@ class Router
 
 						$middlewareInstance = new $middlewareName($request, $response);
 
+						// Throw error if defined method doesn't exist in middleware 
 						if (!method_exists($middlewareInstance, $middlewareFunctionName)) {
 							throw new Exception("Unable to load method: $middlewareFunctionName in $middleware");
 
 							return;
 						}
 
+						// Call middleware method
 						$middlewareInstance->$middlewareFunctionName();
 					}
 				});
 
+				// Run all after middleware sequentially
 				foreach ($route->middlewares->before as $middleware) {
 					$middlewareFunctionName = ResolveMiddleware::ResolveFunctionName($middleware);
 
@@ -53,14 +64,17 @@ class Router
 
 					$middlewareInstance = new $middlewareName($request, $response);
 
+					// Throw error if defined method doesn't exist in middleware 
 					if (!method_exists($middlewareInstance, $middlewareFunctionName)) {
 						throw new Exception("Unable to load method: $middlewareFunctionName in $middleware");
 
 						return;
 					}
 
+					// Call middleware method
 					$middlewareInstance->$middlewareFunctionName();
 
+					// Don't call controller or next before middleware if response ended in current middleware
 					if ($response->ended()) {
 						return;
 					}
@@ -72,19 +86,21 @@ class Router
 
 				$controller = new $controllerName($request, $response);
 
+				// Throw error if defined method doesn't exist in controller 
 				if (!method_exists($controller, $functionName)) {
 					throw new Exception("Unable to load method: $functionName in $route->controller");
 
 					return;
 				}
 
+				// Call controller method
 				$controller->$functionName();
 
 				return;
 			}
 		}
 
-		// 404
-		throw new Exception('No controller for url: ' . $url. ', method: ' . $method);
+		// Throw exception if no controller defined for current endpoint
+		throw new Exception('No controller for url: ' . $url . ', method: ' . $method);
 	}
 }

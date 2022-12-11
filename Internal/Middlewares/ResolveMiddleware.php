c52b48d95b45f510de3f3cdd8eea4c212033d434
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-12-11 23:27:12 +0700

    docs(comments): Internal resolve middleware

--- Internal/Middlewares/ResolveMiddleware.php
+++ Internal/Middlewares/ResolveMiddleware.php
@@ -4,6 +4,11 @@ namespace Internal\Middlewares;
 
 class ResolveMiddleware
 {
+	/**
+	 * Internal use only
+	 * 
+	 * Resolve middleware class
+	 */
 	public static function ResolveComputed(string $controllerString)
 	{
 		$name = explode('::', $controllerString)[0];
@@ -13,6 +18,11 @@ class ResolveMiddleware
 		return $computedName;
 	}
 
+	/**
+	 * Internal use only
+	 * 
+	 * Resolve middleware method to call
+	 */
 	public static function ResolveFunctionName(string $controllerString)
 	{
 		return explode('::', $controllerString)[1];

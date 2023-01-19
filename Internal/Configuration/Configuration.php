cc480d30a00e1506016d99499c0bd213a65b0e53
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-01-19 18:16:17 +0700

    feat: Get env

--- Internal/Configuration/Configuration.php
+++ Internal/Configuration/Configuration.php
@@ -8,6 +8,11 @@ use Internal\Logger\Logger;
 
 class Configuration
 {
+	/**
+	 * Internal use only
+	 *
+	 * Load `.env` file
+	 */
 	public static function LoadEnvFile()
 	{
 		$dotenv = Dotenv::createImmutable(ENV_DIR);
@@ -17,5 +22,17 @@ class Configuration
 		} catch (Exception) {
 			Logger::Log('warning', 'Env file doesn\'t exist');
 		}
+	}
+
+	/**
+	 * Get env
+	 *
+	 * If env doesn't exist it return null
+	 */
+	public static function getEnv(string $key)
+	{
+		if (isset($_ENV[$key])) return $_ENV[$key];
+
+		return null;
 	}
 }

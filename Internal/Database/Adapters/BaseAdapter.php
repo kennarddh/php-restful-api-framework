62b659c4961741675d26c3668e25df602475a72a
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-02-02 18:18:15 +0700

    docs: Comment base adapter

--- Internal/Database/Adapters/BaseAdapter.php
+++ Internal/Database/Adapters/BaseAdapter.php
@@ -9,15 +9,33 @@ namespace Internal\Database\Adapters;
  */
 abstract class BaseAdapter
 {
+	/**
+	 * Connection object
+	 */
 	protected $connection;
 
+	/**
+	 * Connect
+	 */
 	public abstract function __construct(array $data);
 
+	/**
+	 * Select data
+	 */
 	public abstract function Get(string $tableName, array $selects, array $filter): array;
 
+	/**
+	 * Insert new data
+	 */
 	// public abstract function Insert(string $tableName, array $data): bool;
 
+	/**
+	 * Update data
+	 */
 	// public abstract function Update(string $tableName, array $filter, array $data): bool;
 
+	/**
+	 * Delete data
+	 */
 	// public abstract function Delete(string $tableName, array $filter): bool;
 }
